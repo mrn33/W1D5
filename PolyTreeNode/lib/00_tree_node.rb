5a18ec25db9074d4beaf827d5dc916cc73a29bf8
mrn33 <m.najafi@protonmail.com>
2018-06-22 13:47:15 -0700

    Add projects

---
+++ PolyTreeNode/lib/00_tree_node.rb
@@ -0,0 +1,74 @@
+require 'byebug'
+
+class PolyTreeNode
+  # attr_reader :parent, :value, :children
+  
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+  
+  def parent
+    @parent
+  end
+  
+  def children
+    @children
+  end
+  
+  def value
+    @value
+  end
+  
+  def parent=(new_parent)
+    @parent.children.delete(self) unless @parent == nil
+    @parent = new_parent 
+    
+    unless new_parent == nil
+      @parent.children << self unless @parent.children.include?(self)
+    end
+  end 
+  
+  def add_child(child_node)
+    child_node.parent = self
+  end
+  
+  def remove_child(child_node)
+    raise "Node is not a child" unless child_node.parent == self
+    child_node.parent = nil
+  end
+  
+  def dfs(target_value)
+    return self if self.value == target_value
+    children.each do |child| 
+      if child.value == target_value? 
+        return child
+      else
+        child.dfs(target_value) }
+      end
+    end
+    nil
+  end
+  
+  # def dfs(target_value)
+  #   # byebug
+  #   self.value == target_value ? self : nil
+  #   children.each { |child| child.dfs(target_value) }
+  # end
+  
+  def bfs(target_value)
+    queue = []
+    queue.push(self)
+    
+    until queue.empty?
+      current_node = queue.shift
+      return current_node if current_node.value == target_value
+      queue.concat(current_node.children)
+    end
+    
+    nil 
+  end
+  
+  
+end
